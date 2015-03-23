5ef24206af2030d6706e9eb7abbd511fc06dfed1
Kevin Lizander <kevin9075@gmail.com>
2015-03-23 12:02:30 -0400

    Uploading small changes to chapter 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,9 +2,14 @@ def translate(string)
 	if string.start_with?('a','e','i','o','u')
 		string << "ay"
 	else
-		string2 = string
-		tail_string = string2[0..0] + "ay"
-		string.slice!(0)
-		string << tail_string
+		string2 = string.split('')
+		string2.each_with_index do |letter, index|
+			if !letter.start_with?('a','e','i','o','u') && !string2[index - 1].start_with?('a','e','i','o','u')
+				string2.delete(letter)
+				string2.push(letter)
+			end
+			string2 << "ay"
+			string2.join('')
+		end
 	end
 end
