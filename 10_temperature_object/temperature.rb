5407e9f94ce045d4824b8aeeb1cbb8488b8f97a7
Kevin Lizander <kevin9075@gmail.com>
2015-03-26 12:20:03 -0400

    Finished chapter 10 with bonus

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -4,23 +4,23 @@ class Temperature
 
 	def initialize(opts = {})
 		@options = opts
-		@fahrenheit = @options[:f]
-		@celsius = @options[:c]
+		@f = @options[:f]
+		@c = @options[:c]
 	end
 
 	def in_fahrenheit
-		if @celsius == nil
-			return @fahrenheit
-		else
-			return @celsius * (9.0/5.0) + 32
+		if @options.has_key?(:f)
+			return @f
+		elsif @options.has_key?(:c)
+			ftoc(@c)
 		end
 	end
 
 	def in_celsius
-		if @fahrenheit == nil || @fahrenheit == 0
-			return @celsius
-		else
-			return (@fahrenheit - 32) * (5.0/9.0)
+		if @options.has_key?(:c)
+			return @c
+		elsif @options.has_key?(:f)
+			ctof(@f)
 		end
 	end
 
@@ -30,5 +30,25 @@ class Temperature
 
 	def self.from_fahrenheit(fahrenheit)
 		self.new(:f => fahrenheit)
+	end
+
+	def ftoc(num)
+		return num * (9.0/5.0) + 32
+	end
+
+	def ctof(num)
+		return (num - 32) * (5.0/9.0)
+	end
+end
+
+class Celsius < Temperature
+	def initialize(number)
+		super(c: number)
+	end
+end
+
+class Fahrenheit < Temperature
+	def initialize(number)
+		super(f: number)
 	end
 end
