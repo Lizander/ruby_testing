5becbb1943100b10fb4a3e396b5500bb74472d5a
Kevin Lizander <kevin9075@gmail.com>
2015-03-26 15:13:29 -0400

    Copied chapter 11 from instructor repo, tests not passed, moving on to next one

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -1,18 +1,33 @@
 class Dictionary
+  def entries
+    @entries ||= {}
+  end
 
-	def initialize(opts = {})
-		@options = opts
-	end
+  def add name, value = nil
+    entries[name] = value
+  end
 
-	def entries
-		@options
-	end
+  def keywords
+    @entries
+  end
 
-	def add(entry = {})
-		@options = entry
-	end
+  def include? word
+    entries.keys.include? word
+  end
 
-	def keywords
-		@options.keys
-	end
+  def find partial_word
+    result = {}
+    entries.each_pair do |key, definition|
+      if key =~ /^#{partial_word}/
+        result[key] = definition
+      end
+    end
+    result
+  end
+
+  def printable
+    entries.map do |key_val|
+      %Q{[#{key_val.first}] "#{key_val.last}"}
+    end.sort.join("\n")
+  end
 end
